591fe255961e73d6f6f5785ee1f1310e0ebadf4c
sam-b02 <samarbaskar02@gmail.com>
2025-05-28 17:41:09 +0530

    add regex to split into arrays

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -3,4 +3,8 @@ import json
 
 with open(r"output\text.txt", "r", encoding="utf-8") as file:
     rawdata = file.read()
-    
+
+museums = []
+
+museums_sections = re.split(r'\n(?=\d+[a-zA-Z]?\.\d+)', rawdata) #takes the raw data and splits into sections based on the starting character (digit,dot,digit)
+
