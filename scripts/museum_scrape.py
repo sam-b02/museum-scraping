2a34f91ad70b84c800fcadee76e3fcd73ca288df
sam-b02 <samarbaskar02@gmail.com>
2025-05-28 17:07:54 +0530

    page range update

--- scripts/museum_scrape.py
+++ scripts/museum_scrape.py
@@ -2,9 +2,9 @@ import pymupdf
 
 doc = pymupdf.open(r"data\museums.pdf") # open a document
 
-out = open("output\text.txt", "wb") 
+out = open(r"output\text.txt", "wb") 
 for x, page in enumerate(doc):
-    if x >= 16: 
+    if x >= 16 and x <= 796: 
         text = page.get_text().encode("utf8") 
         out.write(text)
         out.write(bytes((12,)))
